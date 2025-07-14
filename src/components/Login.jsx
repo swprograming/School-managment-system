674449bdcd4354f7558b7723882fa8c75d69a481
Solomon Wondwesen <101749398+swprograming@users.noreply.github.com>
2025-07-14 15:59:10 +0300

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     setError(""); // Clear previous error messages
 
     try {
-      const response = await fetch("https://school-managment-systembk.onrender.com/", {
+      const response = await fetch("https://school-managment-systembk.onrender.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
